66f32f2cc5a724d1d5647f9682ad075237830088
jeanne <jeanne.ginzburg@epfedu.fr>
2022-02-28 15:48:17 +0100

    cliquer sur 1 bouton

--- app/src/main/java/com/example/projetandroid/MainActivity.java
+++ app/src/main/java/com/example/projetandroid/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.projetandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button btn_Alpes;
+    private Button btn_atlantique;
+    private Button btn_cotedazur;
+    private Button btn_Paris;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+       btn_Alpes.setOnClickListener{// regarde à chaque fois qu'on clique sur le bouton
+
+        }
+
+
     }
 }
