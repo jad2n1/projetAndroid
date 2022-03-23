52b599aa4b913e8405cd7b1247d50312d8840f47
juliettetort <juliette.tort@epfedu.fr>
2022-03-23 16:46:22 +0100

    test pour appuyer sur le bouton Pas fonctionnel

--- app/src/main/java/com/example/projetandroid/ecran2.java
+++ app/src/main/java/com/example/projetandroid/ecran2.java
@@ -10,5 +10,6 @@ public class ecran2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ecran2);
+
     }
 }
