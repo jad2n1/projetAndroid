19b48e73779317ef4058b4ae791266b4464a6ac5
jadedehaynin <jade.dehaynin@epfedu.fr>
2022-04-24 12:47:14 +0200

    bouton Alpes figé

--- app/src/main/java/com/example/projetandroid/MainActivity.java
+++ app/src/main/java/com/example/projetandroid/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.projetandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
